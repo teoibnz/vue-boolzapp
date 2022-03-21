1b1b0ec45b905eadac4f48b37e3e23abc50f8568
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-21 15:06:17 +0100

    inserimento img avatar

--- main.js
+++ main.js
@@ -1,13 +1,60 @@
-// const app = new Vue (
-//     {
-//         el : '#app',
+const app = new Vue (
+    {
+        el : '#app',
         
-//         data :'',
+        data : {
+            contacts : [
+                {
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+            ],
+        },
         
-//         methods : {
+        // methods : {
             
-//         },
+        // },
         
-// } 
-// );
+}
+);
 
