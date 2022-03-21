70e63ee21f7ae3c3062bae6e0319f82b4085f6e9
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-21 15:38:49 +0100

    inserimento dinamico degli avatar con un v-for

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = new Vue (
             contacts : [
                 {
                     name: 'Michele',
-                    avatar: '_1',
+                    avatar: 'img/avatar_1.jpg',
                     visible: true,
                     messages: [
                         {
@@ -28,7 +28,7 @@ const app = new Vue (
                 },
                 {
                     name: 'Fabio',
-                    avatar: '_2',
+                    avatar: 'img/avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
@@ -48,6 +48,63 @@ const app = new Vue (
                         }
                     ],
                 },
+                {
+                    name: 'Samuele',
+                    avatar: 'img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro B.',
+                    avatar: 'img/avatar_4.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro L.',
+                    avatar: 'img/avatar_5.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ricordati di chiamare la nonna',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Va bene, stasera la sento',
+                            status: 'received'
+                        }
+                    ],
+                },
+            
             ],
         },
         
